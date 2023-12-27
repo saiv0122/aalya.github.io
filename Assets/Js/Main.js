657f76eb0fbf7f6fa624a4c71020bbe5d32e6a3f
saiv0122 <154981067+saiv0122@users.noreply.github.com>
2023-12-27 17:48:02 +0530

    Add files via upload

---
+++ Assets/Js/Main.js
@@ -0,0 +1,10 @@
+function NextPage() {
+    window.location.href = "Yes.html";
+}
+
+function MoveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight);
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
